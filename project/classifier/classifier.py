7b0f93268b676acbe1a59b39134056b79d5de43a
George Oliveira <ghao@cin.ufpe.br>
2016-09-15 14:10:10 -0300

    more classifiers

--- project/classifier/classifier.py
+++ project/classifier/classifier.py
@@ -7,6 +7,9 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.naive_bayes import GaussianNB
 from time import time
 
 LINK_FILES = ["rotten.txt", "imdb.txt", "metacritic.txt", "movies.txt", "allmovies.txt", "flixter.txt", "tribute.txt", "boxofficemojo.txt", "mubi.txt", "yifi.txt"]
@@ -70,7 +73,7 @@ def print_scores(scores):
     print "F1-Measure: %f\n" % scores['f1']
 
 def try_svm(features_train, features_test, labels_train, labels_test):
-    print "Testing SVMs:"
+    print "Testing SVMs:\n-------------\n"
 
     C = [1, 10, 20, 100, 1000, 10000]
     kernels = ['rbf', 'linear', 'poly', 'sigmoid']
@@ -87,17 +90,43 @@ def try_svm(features_train, features_test, labels_train, labels_test):
             scores = evaluate(labels_test, clf.predict(features_test))
             print_scores(scores)
 
-def try_mlp(features_train, features_test, labels_train, labels_test):
-    #TODO
-    pass
-
 def try_random_forest(features_train, features_test, labels_train, labels_test):
-    #TODO
-    pass
+    print "Testing Random Forests:\n-----------------------\n"
+
+    N = [10, 20, 100, 200, 300, 10000]
+
+    for n in N:
+        print "N=%d" % n
+
+        t0 = time()
+        clf = RandomForestClassifier(n_estimators = n)
+        clf.fit(features_train, labels_train)
+        print "Training time %fs" % round(time() - t0, 3)
+
+        scores = evaluate(labels_test, clf.predict(features_test))
+        print_scores(scores)
 
 def try_regression(features_train, features_test, labels_train, labels_test):
-    #TODO
-    pass
+    print "Testing Linear Regression:\n--------------------------\n"
+
+    t0 = time()
+    clf = LinearRegression()
+    clf.fit(features_train, labels_train)
+    print "Training time %fs" % round(time() - t0, 3)
+
+    print "Naive score %f" % clf.score(features_train, labels_train)
+    print "R squared score %f\n" % clf.score(features_test, labels_test)
+
+def try_naive_bayes(features_train, features_test, labels_train, labels_test):
+    print "Testing Naive Bayes:\n--------------------\n"
+
+    t0 = time()
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    print "Training time %fs" % round(time() - t0, 3)
+
+    scores = evaluate(labels_test, clf.predict(features_test))
+    print_scores(scores)
 
 if __name__ == '__main__':
     db = load_database()
@@ -108,4 +137,7 @@ if __name__ == '__main__':
 
     features_train, features_test, labels_train, labels_test = split_dataset(bag_of_words_vectors, Y)
 
+    try_naive_bayes(features_train, features_test, labels_train, labels_test)
+    try_regression(features_train, features_test, labels_train, labels_test)
+    try_random_forest(features_train, features_test, labels_train, labels_test)
     try_svm(features_train, features_test, labels_train, labels_test)
