28e9059990c46d43a43e23ba21f6f35f5f77a7f8
George Oliveira <ghao@cin.ufpe.br>
2016-09-22 20:59:26 -0300

    Classifier class using the best classifier from the experiments

--- project/classifier/classifier.py
+++ project/classifier/classifier.py
@@ -10,17 +10,15 @@ from sklearn.neighbors import KNeighborsClassifier
 
 class Classifier(object):
     def __init__(self):
-        self._classifier = None # Need to define which classifier will be used
+        self._classifier = AdaBoostClassifier(n_estimators=1000, learning_rate=0.5) # Best classifier
         self._database = load_database()
         self._X, self._Y = prepare_database(self._database)
 
-        self._vocabulary = None # Create bag of words or TF-IDF model
-        self._features_train, self._features_test, self._labels_train, self._labels_test = split_dataset(self._vocabulary, self._Y)
+        self._vocabulary, self._vocabulary_vectors = create_bag_of_words(self._X) # Creates Bag of Words model
+        self._features_train, self._features_test, self._labels_train, self._labels_test = split_dataset(self._vocabulary_vectors, self._Y)
 
         self._classifier.fit(self._features_train, self._labels_train)
         self._scores = evaluate(self._labels_test, self._classifier.predict(self._features_test))
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.neighbors import KNeighborsClassifier
 
     def classify(self, html):
         text = extract_text(html)
@@ -30,3 +28,28 @@ from sklearn.neighbors import KNeighborsClassifier
 
     def get_scores(self):
         return self._scores
+
+def _test():
+    import urllib2
+
+    USER_AGENT = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+                  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+
+    url = 'https://www.rottentomatoes.com/m/tideland'
+    page = urllib2.Request(url, headers=USER_AGENT)
+    page = urllib2.urlopen(page)
+    html_text = page.read()
+
+    clf = Classifier()
+    scores = clf.get_scores()
+    result = 'Is relevant' if clf.classify(html_text) == 1 else 'Is not relevant'
+
+    print "Accuracy: %f" % scores['accuracy']
+    print "Precision: %f" % scores['precision']
+    print "Recall: %f" % scores['recall']
+    print "F1-Measure: %f" % scores['f1']
+
+    print "\nResult for url %s: '%s'" % (url, result)
+
+if __name__ == '__main__':
+    _test()
