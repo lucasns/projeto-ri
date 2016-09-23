e11610222b7df96654faa7a33ed8d40bac0c57e9
nogn <Nicolas94@Nicolass-MacBook-Pro.local>
2016-09-23 19:11:03 -0300

    Add robots.txt to each website and use cpickle to store saved pages

--- project/crawler/crawler.py
+++ project/crawler/crawler.py
@@ -4,63 +4,108 @@ import requests
 import time
 import re
 import os.path
-import pickle
+import cPickle as pickle
 from urlparse import urljoin
 from bs4 import BeautifulSoup
 
 
 DOMAINS = { 
             'rottentomatoes':   {'seed': 'https://www.rottentomatoes.com/',
-                                 'robots_txt': ['license/export/', 'search', 'user', 'click', 'logout', 'trailer', 'login', 'ajax'],
+                                 'robots_txt': ['license/export/', 'search', 'user', 'click', 
+                                                'logout', 'trailer', 'login', 'ajax'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'https://www.rottentomatoes.com/m/.*'
                                 },
+
             'imdb':             {'seed': 'http://www.imdb.com/',
-                                 'robots_txt': [],
+                                 'robots_txt': ['tvschedule', 'ActorSearch', 'ActressSearch', 'AddRecommendation', 
+                                                'ads/', 'AlternateVersions', 'AName', 'Awards', 'BAgent', 'Ballot/', 
+                                                'BornInYear', 'BornWhere', 'BPublicity', 'BQuotes', 'BTrivia', 
+                                                'BusinessThisDay', 'BWorks', 'careers', 'help/show_leaf?careeratimdb', 
+                                                'CommentsAuthor', 'CommentsEnter', 'CommentsIndex', 'Companies', 
+                                                'CrazyCredits', 'Credits', 'DiedInYear', 'DiedWhere', 'DVD', 'ExciteTitle', 
+                                                'Find', 'FName', 'GName', 'Guests', 'harvest_me', 'HelpPage', 'Icons/', 
+                                                'JointVentures', 'Laserdisc', 'List', 'Literature', 'Locations', 
+                                                'LocationTree', 'Lookup', 'M/', 'Maltin', 'MarriedInYear', 'MetaSearch', 
+                                                'Mlinks', 'More', 'Movies', 'Movies/', 'MyMovies', 'mymovies/', 
+                                                'name_pick_n_mix', 'Nsearch', 'NUrls', 'OnThisDay', 'Ontv', 'OnTV', 
+                                                'Overlap', 'Pawards', 'pick_n_mix', 'PName', 'Posters', 'prepare_data', 
+                                                'Psales', 'Quiz', 'r/', 'ra/', 'Ratings', 'rd/', 'Recommendations', 
+                                                'register', 'ReleaseDates', 'ReleasedInYear', 'Reviews', 'rg/', 'ri/', 
+                                                'RName', 'Sales', 'SearchAwards', 'SearchBios', 'SearchBusiness', 
+                                                'SearchCrazy', 'SearchDVD', 'SearchGoofs', 'SearchLaserdisc', 
+                                                'SearchLiterature', 'SearchPlots', 'SearchPlotWriters', 'SearchQuotes', 
+                                                'SearchRatios', 'SearchSongs', 'SearchTaglines', 'SearchTechnical', 
+                                                'SearchTrivia', 'SearchVersions', 'ShowAll', 'Showing', 'SName', 
+                                                'Soundtracks', 'Taglines', 'Tawards', 'Technical', 'tiger_redirect', 
+                                                'Title/ASIN', 'TitleBrowse', 'Trailers', 'Tsearch', 'TUrls', 'VName', 
+                                                'Vote', 'WorkedWith', 'updates', 'board', 'boards'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'http://www.imdb.com/title/.*'
                                 },
+
             'metacritic':       {'seed': 'http://www.metacritic.com/',
-                                 'robots_txt': [],
+                                 'robots_txt': ['search', 'signup', 'login', 'user', 'jl/'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'http://www.metacritic.com/movie/.*'
                                 },
+
             'movies':           {'seed': 'http://www.movies.com/',
-                                 'robots_txt': [],
+                                 'robots_txt': ['undefined', 'Bin/', 'backlot/', 'controls/', 'reviews/controls/', 
+                                                'exclusives/controls/','search', 'narrow-your-search-results', 
+                                                'search-results', 'search-result', 'page-not-found'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'http://www.movies.com/.*/m.*'
                                 },
+
             'allmovie':         {'seed': 'http://www.allmovie.com/',
                                  'robots_txt': ['admin', 'user/'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'http://www.allmovie.com/movie/.*'
                                 },
+
             'flixster':         {'seed': 'https://www.flixster.com/',
-                                 'robots_txt': [],
+                                 'robots_txt': ['actor/random', 'admin', 'address-book', 'api', 'bebo', 'captcha', 
+                                                 'converse.do', 'DoNotSend.jsp', 'facebook', 'friendsAction.do', 
+                                                 'friends.do', 'global-scripts', 'hi5', 'igoogle', 'invite', 
+                                                 'inviteDisplay.do', 'mctResults.do', 'metrics', 'mmessage', 
+                                                 'movie-facts', 'myspace', 'news/saved-stories', 
+                                                 'news/submitted-stories', 'page-scripts', 'people', 
+                                                 'people.do', 'rate-movies', 'recommendations', 'static/scripts', 
+                                                 'support', 'userAuth.do', 'ad.do', 'sponsors', 'misc', 
+                                                 'email-content', 'static/ads', 'user/'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'https://www.flixster.com/movie/.*'
                                 },  
+
             'tribute':          {'seed': 'http://www.tribute.ca/',
-                                 'robots_txt': [],
+                                 'robots_txt': ['demo/', 'tvlistings/', 'ads/', 'doubleclick/', 'flash/', 'feeds/', 
+                                                'controls/', 'aspnet_client/', 'pages/', 'app_data/', 'css/', 
+                                                'lib/', 'config/', 'aspnet_client/', 'rma/'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'http://www.tribute.ca/movies/.*'
                                 },
+
             'boxofficemojo':    {'seed': 'http://www.boxofficemojo.com/',
-                                 'robots_txt': [],
+                                 'robots_txt': ['movies/default.movies.htm', 'showtimes/buy.php', 'forums/', 
+                                 'derbygame/', 'grades/', 'moviehangman/', 'users/'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'http://www.boxofficemojo.com/movies/?id=.*'
                                 },
+
             'mubi':             {'seed': 'https://mubi.com/',
-                                 'robots_txt': [],
+                                 'robots_txt': ['login'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'https://mubi.com/films.*'
                                 },
+
             'yify':             {'seed': 'http://yify.info/',
-                                 'robots_txt': [],
+                                 'robots_txt': ['engine/go.php', 'engine/download.php', 'user/', 'newposts/', 
+                                                'statistics.html'],
                                  'sleep_time': 1,
                                  'movies_pattern': r'http://yify.info/.*'
                                 }
-}
+        }
 
 USER_AGENT = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
@@ -130,16 +175,16 @@ class Crawler(threading.Thread):
                 if "text/html" not in r.headers["content-type"]:
                     continue
 
+                self.crawled_websites.append(html_text)
+                print(current_url)
+
+                if not self.stop_expanding:
+                    self.expand_page(html_text)
+
             except Exception,e:
                 print("\nERROR WITH URL: " + current_url)
                 print("ERROR MESSAGE: " + e.message + '\n')
-            
-            self.crawled_websites.append(html_text)
-            print(current_url)
-
-            if not self.stop_expanding:
-                self.expand_page(html_text)
-
+          
 
 def export_all_crawled_pages():
     with open('results.pickle', 'rb') as f:
