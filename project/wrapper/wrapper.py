7008308c634ad37bde46594f292243747bcfa070
Lucas Nunes <lns2@cin.ufpe.br>
2016-09-25 21:59:12 -0300

    Create MovieInfo class

---
+++ project/wrapper/wrapper.py
@@ -0,0 +1,38 @@
+from specific import *
+from bs4 import BeautifulSoup
+
+
+class MovieInfo(object):
+    def __init__(self, info=[]):
+        self.site = None
+        self.title = None
+        self.synopsis = None
+        self.rating = None
+        self.genre = None
+        self.director = None
+        self.date = None
+        self.box_office = None
+        self.runtime = None
+
+        if list:
+            self.set_info(info)
+        
+
+    def get_info(self):
+        return self.site, self.title, self.synopsis, self.rating, self.genre, self.director, self.date, self.box_office, self.runtime
+
+    
+    def set_info(self, info_list):
+        self.site, self.title, self.synopsis, self.rating, self.genre, self.director, self.date, self.box_office, self.runtime = info_list
+
+
+    def __repr__(self):
+        return "\n".join(("Site: " + str(self.site),
+                          "Title: " + str(self.title),
+                          "Synopsis: " + str(self.synopsis),
+                          "MPPA Rating: " + str(self.rating),
+                          "Genre: " + str(None) if self.genre is None else ", ".join(self.genre),
+                          "Director: " + str(None) if self.director is None else ", ".join(self.director),
+                          "Release Date: " + str(self.date),
+                          "Box Office: " + str(self.box_office),
+                          "Runtime: " +  str(self.runtime)))
