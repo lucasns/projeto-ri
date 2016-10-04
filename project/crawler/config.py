df1a81f37471ad9ff1db3df503e2a339048291af
George Oliveira <ghao@cin.ufpe.br>
2016-10-04 19:59:52 -0300

    Fix yify heuristic and create config file

---
+++ project/crawler/config.py
@@ -0,0 +1,110 @@
+import os
+
+DOMAINS = {
+            'rottentomatoes':   {'seed': 'https://www.rottentomatoes.com/',
+                                 'robots_txt': ['license/export/', 'search', 'user', 'click',
+                                                'logout', 'trailer', 'login', 'ajax'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'https://www.rottentomatoes.com/m/.*/$'
+                                },
+
+            'imdb':             {'seed': 'http://www.imdb.com/',
+                                 'robots_txt': ['tvschedule', 'ActorSearch', 'ActressSearch', 'AddRecommendation',
+                                                'ads/', 'AlternateVersions', 'AName', 'Awards', 'BAgent', 'Ballot/',
+                                                'BornInYear', 'BornWhere', 'BPublicity', 'BQuotes', 'BTrivia',
+                                                'BusinessThisDay', 'BWorks', 'careers', 'help/show_leaf?careeratimdb',
+                                                'CommentsAuthor', 'CommentsEnter', 'CommentsIndex', 'Companies',
+                                                'CrazyCredits', 'Credits', 'DiedInYear', 'DiedWhere', 'DVD', 'ExciteTitle',
+                                                'Find', 'FName', 'GName', 'Guests', 'harvest_me', 'HelpPage', 'Icons/',
+                                                'JointVentures', 'Laserdisc', 'List', 'Literature', 'Locations',
+                                                'LocationTree', 'Lookup', 'M/', 'Maltin', 'MarriedInYear', 'MetaSearch',
+                                                'Mlinks', 'More', 'Movies', 'Movies/', 'MyMovies', 'mymovies/',
+                                                'name_pick_n_mix', 'Nsearch', 'NUrls', 'OnThisDay', 'Ontv', 'OnTV',
+                                                'Overlap', 'Pawards', 'pick_n_mix', 'PName', 'Posters', 'prepare_data',
+                                                'Psales', 'Quiz', 'r/', 'ra/', 'Ratings', 'rd/', 'Recommendations',
+                                                'register', 'ReleaseDates', 'ReleasedInYear', 'Reviews', 'rg/', 'ri/',
+                                                'RName', 'Sales', 'SearchAwards', 'SearchBios', 'SearchBusiness',
+                                                'SearchCrazy', 'SearchDVD', 'SearchGoofs', 'SearchLaserdisc',
+                                                'SearchLiterature', 'SearchPlots', 'SearchPlotWriters', 'SearchQuotes',
+                                                'SearchRatios', 'SearchSongs', 'SearchTaglines', 'SearchTechnical',
+                                                'SearchTrivia', 'SearchVersions', 'ShowAll', 'Showing', 'SName',
+                                                'Soundtracks', 'Taglines', 'Tawards', 'Technical', 'tiger_redirect',
+                                                'Title/ASIN', 'TitleBrowse', 'Trailers', 'Tsearch', 'TUrls', 'VName',
+                                                'Vote', 'WorkedWith', 'updates', 'board', 'boards'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'http://www.imdb.com/title/.*/'
+                                },
+
+            'metacritic':       {'seed': 'http://www.metacritic.com/',
+                                 'robots_txt': ['search', 'signup', 'login', 'user', 'jl/'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'http://www.metacritic.com/movie/[^/\?]*$'
+                                },
+
+            'movies':           {'seed': 'http://www.movies.com/',
+                                 'robots_txt': ['undefined', 'Bin/', 'backlot/', 'controls/', 'reviews/controls/',
+                                                'exclusives/controls/','search', 'narrow-your-search-results',
+                                                'search-results', 'search-result', 'page-not-found'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'http://www.movies.com/[^/\?]*/m[0-9]*$'
+                                },
+
+            'allmovie':         {'seed': 'http://www.allmovie.com/',
+                                 'robots_txt': ['admin', 'user/'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'http://www.allmovie.com/movie/[^/\?]*$'
+                                },
+
+            'flixster':         {'seed': 'https://www.flixster.com/',
+                                 'robots_txt': ['actor/random', 'admin', 'address-book', 'api', 'bebo', 'captcha',
+                                                 'converse.do', 'DoNotSend.jsp', 'facebook', 'friendsAction.do',
+                                                 'friends.do', 'global-scripts', 'hi5', 'igoogle', 'invite',
+                                                 'inviteDisplay.do', 'mctResults.do', 'metrics', 'mmessage',
+                                                 'movie-facts', 'myspace', 'news/saved-stories',
+                                                 'news/submitted-stories', 'page-scripts', 'people',
+                                                 'people.do', 'rate-movies', 'recommendations', 'static/scripts',
+                                                 'support', 'userAuth.do', 'ad.do', 'sponsors', 'misc',
+                                                 'email-content', 'static/ads', 'user/'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'https://www.flixster.com/movie/[^/\?]*$'
+                                },
+
+            'tribute':          {'seed': 'http://www.tribute.ca/',
+                                 'robots_txt': ['demo/', 'tvlistings/', 'ads/', 'doubleclick/', 'flash/', 'feeds/',
+                                                'controls/', 'aspnet_client/', 'pages/', 'app_data/', 'css/',
+                                                'lib/', 'config/', 'aspnet_client/', 'rma/'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'http://www.tribute.ca/movies/[^/\?]*/[0-9]*/$'
+                                },
+
+            'boxofficemojo':    {'seed': 'http://www.boxofficemojo.com/',
+                                 'robots_txt': ['movies/default.movies.htm', 'showtimes/buy.php', 'forums/',
+                                 'derbygame/', 'grades/', 'moviehangman/', 'users/'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'http://www.boxofficemojo.com/movies/\?id=.*'
+                                },
+
+            'mubi':             {'seed': 'https://mubi.com/',
+                                 'robots_txt': ['login'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'https://mubi.com/films/*[^/\?]*$'
+                                },
+
+            'yify':             {'seed': 'http://yify.info/',
+                                 'robots_txt': ['engine/go.php', 'engine/download.php', 'user/', 'newposts/',
+                                                'statistics.html'],
+                                 'sleep_time': 1,
+                                 'movies_pattern': r'http://yify.info/[^(?!tv$)].*/[0-9]*[-].*.html$'
+                                }
+        }
+
+
+USER_AGENT = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+                      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+FILES_PATH = os.path.join(DIR_PATH, "files")
+
+if not os.path.exists(FILES_PATH):
+    os.makedirs(FILES_PATH)
--- project/crawler/crawler.py
+++ project/crawler/crawler.py
@@ -1,119 +1,23 @@
-import urllib2
-import threading
-import requests
-import time
-import re
-import os
-import cPickle as pickle
-from urlparse import urljoin
 from bs4 import BeautifulSoup
+import cPickle as pickle
+import re
+import requests
+import os
+import threading
+import time
+import urllib2
+from urlparse import urljoin
 
-
-DOMAINS = {
-            'rottentomatoes':   {'seed': 'https://www.rottentomatoes.com/',
-                                 'robots_txt': ['license/export/', 'search', 'user', 'click',
-                                                'logout', 'trailer', 'login', 'ajax'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'https://www.rottentomatoes.com/m/.*/$'
-                                },
-
-            'imdb':             {'seed': 'http://www.imdb.com/',
-                                 'robots_txt': ['tvschedule', 'ActorSearch', 'ActressSearch', 'AddRecommendation',
-                                                'ads/', 'AlternateVersions', 'AName', 'Awards', 'BAgent', 'Ballot/',
-                                                'BornInYear', 'BornWhere', 'BPublicity', 'BQuotes', 'BTrivia',
-                                                'BusinessThisDay', 'BWorks', 'careers', 'help/show_leaf?careeratimdb',
-                                                'CommentsAuthor', 'CommentsEnter', 'CommentsIndex', 'Companies',
-                                                'CrazyCredits', 'Credits', 'DiedInYear', 'DiedWhere', 'DVD', 'ExciteTitle',
-                                                'Find', 'FName', 'GName', 'Guests', 'harvest_me', 'HelpPage', 'Icons/',
-                                                'JointVentures', 'Laserdisc', 'List', 'Literature', 'Locations',
-                                                'LocationTree', 'Lookup', 'M/', 'Maltin', 'MarriedInYear', 'MetaSearch',
-                                                'Mlinks', 'More', 'Movies', 'Movies/', 'MyMovies', 'mymovies/',
-                                                'name_pick_n_mix', 'Nsearch', 'NUrls', 'OnThisDay', 'Ontv', 'OnTV',
-                                                'Overlap', 'Pawards', 'pick_n_mix', 'PName', 'Posters', 'prepare_data',
-                                                'Psales', 'Quiz', 'r/', 'ra/', 'Ratings', 'rd/', 'Recommendations',
-                                                'register', 'ReleaseDates', 'ReleasedInYear', 'Reviews', 'rg/', 'ri/',
-                                                'RName', 'Sales', 'SearchAwards', 'SearchBios', 'SearchBusiness',
-                                                'SearchCrazy', 'SearchDVD', 'SearchGoofs', 'SearchLaserdisc',
-                                                'SearchLiterature', 'SearchPlots', 'SearchPlotWriters', 'SearchQuotes',
-                                                'SearchRatios', 'SearchSongs', 'SearchTaglines', 'SearchTechnical',
-                                                'SearchTrivia', 'SearchVersions', 'ShowAll', 'Showing', 'SName',
-                                                'Soundtracks', 'Taglines', 'Tawards', 'Technical', 'tiger_redirect',
-                                                'Title/ASIN', 'TitleBrowse', 'Trailers', 'Tsearch', 'TUrls', 'VName',
-                                                'Vote', 'WorkedWith', 'updates', 'board', 'boards'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'http://www.imdb.com/title/.*/'
-                                },
-
-            'metacritic':       {'seed': 'http://www.metacritic.com/',
-                                 'robots_txt': ['search', 'signup', 'login', 'user', 'jl/'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'http://www.metacritic.com/movie/[^/\?]*$'
-                                },
-
-            'movies':           {'seed': 'http://www.movies.com/',
-                                 'robots_txt': ['undefined', 'Bin/', 'backlot/', 'controls/', 'reviews/controls/',
-                                                'exclusives/controls/','search', 'narrow-your-search-results',
-                                                'search-results', 'search-result', 'page-not-found'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'http://www.movies.com/[^/\?]*/m[0-9]*$'
-                                },
-
-            'allmovie':         {'seed': 'http://www.allmovie.com/',
-                                 'robots_txt': ['admin', 'user/'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'http://www.allmovie.com/movie/[^/\?]*$'
-                                },
-
-            'flixster':         {'seed': 'https://www.flixster.com/',
-                                 'robots_txt': ['actor/random', 'admin', 'address-book', 'api', 'bebo', 'captcha',
-                                                 'converse.do', 'DoNotSend.jsp', 'facebook', 'friendsAction.do',
-                                                 'friends.do', 'global-scripts', 'hi5', 'igoogle', 'invite',
-                                                 'inviteDisplay.do', 'mctResults.do', 'metrics', 'mmessage',
-                                                 'movie-facts', 'myspace', 'news/saved-stories',
-                                                 'news/submitted-stories', 'page-scripts', 'people',
-                                                 'people.do', 'rate-movies', 'recommendations', 'static/scripts',
-                                                 'support', 'userAuth.do', 'ad.do', 'sponsors', 'misc',
-                                                 'email-content', 'static/ads', 'user/'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'https://www.flixster.com/movie/[^/\?]*$'
-                                },
-
-            'tribute':          {'seed': 'http://www.tribute.ca/',
-                                 'robots_txt': ['demo/', 'tvlistings/', 'ads/', 'doubleclick/', 'flash/', 'feeds/',
-                                                'controls/', 'aspnet_client/', 'pages/', 'app_data/', 'css/',
-                                                'lib/', 'config/', 'aspnet_client/', 'rma/'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'http://www.tribute.ca/movies/[^/\?]*/[0-9]*/$'
-                                },
-
-            'boxofficemojo':    {'seed': 'http://www.boxofficemojo.com/',
-                                 'robots_txt': ['movies/default.movies.htm', 'showtimes/buy.php', 'forums/',
-                                 'derbygame/', 'grades/', 'moviehangman/', 'users/'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'http://www.boxofficemojo.com/movies/\?id=.*'
-                                },
-
-            'mubi':             {'seed': 'https://mubi.com/',
-                                 'robots_txt': ['login'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'https://mubi.com/films/*[^/\?]*$'
-                                },
-
-            'yify':             {'seed': 'http://yify.info/',
-                                 'robots_txt': ['engine/go.php', 'engine/download.php', 'user/', 'newposts/',
-                                                'statistics.html'],
-                                 'sleep_time': 1,
-                                 'movies_pattern': r'http://yify.info/.*'
-                                }
-        }
+from config import DOMAINS, USER_AGENT, FILES_PATH
 
 
 class Crawler(threading.Thread):
-    def __init__(self, website_name, website_info, use_heuristic=False):
+    def __init__(self, website_name, website_info, max_pages=500, use_heuristic=False):
         threading.Thread.__init__(self)
         self.website_name = website_name
         self.website_info = website_info
         self.use_heuristic = use_heuristic
+        self.max_pages = max_pages
         self.crawled_websites = []
         self.frontier = [website_info['seed']]
         self.visited = [website_info['seed']]
@@ -123,56 +27,51 @@ class Crawler(threading.Thread):
 
     def match_heuristic(self, url):
         return re.match(self.website_info['movies_pattern'], url)
-    
+        
 
     def is_html(self, url):
         r = requests.head(url)
         return "text/html" in r.headers["content-type"]
-       
-     
-    def get_html(self, url):
-        user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-                      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
 
-        page = urllib2.Request(url, headers=user_agent)
+
+    def get_html(self, url):
+        page = urllib2.Request(url, headers=USER_AGENT)
         page = urllib2.urlopen(page)
         html = page.read()
-        
+
         return html
 
-    
+
     def get_urls(self, html):
         soup = BeautifulSoup(html, "lxml")
         anchor_tags = soup.find_all('a', href=True)
-        
-        urls = [urljoin(self.website_info['seed'], link['href']) for link in anchor_tags]
+
+        urls = [urljoin(self.website_info['seed'], link['href'].lower()) for link in anchor_tags]
 
         urls = [u for u in urls if (self.website_info['seed'] in u and
                                     u not in self.disallowed_links)]
-        
+
         return urls
 
 
     def run(self):
-        max_pages = 500
-
-        while len(self.frontier) > 0 and len(self.crawled_websites) < max_pages:
+        while len(self.frontier) > 0 and len(self.crawled_websites) < self.max_pages:
             index = 0
             if self.use_heuristic:
                 for i, url in enumerate(self.frontier):
                     if self.match_heuristic(url):
                         index = i
                         break
-            
+
             curr_url = self.frontier.pop(index)
             time.sleep(self.website_info['sleep_time'])
 
             try:
                 if not self.is_html(curr_url):
                     continue
-                
+
                 html = self.get_html(curr_url)
-                
+
                 self.crawled_websites.append(html)
                 print(curr_url)
 
@@ -190,24 +89,14 @@ class Crawler(threading.Thread):
         results[self.website_name] = self.crawled_websites
 
 
-def get_files_path():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    files_path = os.path.join(dir_path, 'files')
-
-    if not os.path.exists(files_path):
-        os.makedirs(files_path)
-
-    return files_path
-
-
 def export_all_crawled_pages():
-    files_path = get_files_path()
+    path = FILES_PATH
 
-    with open(os.path.join(files_path, 'results.pickle'), 'rb') as f:
+    with open(os.path.join(path, 'results.pickle'), 'rb') as f:
         results = pickle.load(f)
 
     for website_name, crawled_pages in results.items():
-        folder_path = os.path.join(files_path, website_name)
+        folder_path = os.path.join(path, website_name)
 
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
@@ -217,12 +106,12 @@ def export_all_crawled_pages():
 
             with open(file_complete_name, "w") as f:
                 f.write(html)
-           
+
 
 def crawl():
     crawlers = []
     for website_name, website_info in DOMAINS.iteritems():
-        c = Crawler(website_name, website_info, True)
+        c = Crawler(website_name, website_info, 500, True)
         crawlers.append(c)
         c.start()
 
@@ -232,7 +121,7 @@ def crawl():
         c.join()
         c.save_crawled_pages(results)
 
-    with open(os.path.join(get_files_path(), 'results.pickle'), 'wb') as f:
+    with open(os.path.join(FILES_PATH, 'results.pickle'), 'wb') as f:
         pickle.dump(results, f)
 
 
