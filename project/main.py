38ed77655e3744f8c76ff162ad3085e8b4154f58
George Oliveira <ghao@cin.ufpe.br>
2016-10-17 19:22:09 -0300

    Add Harvest Ratio results

--- project/main.py
+++ project/main.py
@@ -11,8 +11,6 @@ def export_all_crawled_pages(filename):
     results = {}
 
     for website_name in DOMAINS.keys():
-        print(website_name)
-
         folder_path = os.path.join(path, website_name)
 
         if not os.path.exists(folder_path):
@@ -52,25 +50,38 @@ def crawl(use_heuristic, filename):
 
     export_all_crawled_pages(filename)
 
-def harvest_ratio(pages, classifier):
-    total = 0
-    relevant = 0
-
-    for domain in pages.keys():
-        total += len(pages[domain])
-        for p in pages[domain]:
-            if classifier.classify(p):
-                relevant += 1
-
-    hr =  float(relevant) / total
-
-    return hr
-
 def save_csv(filename, rows):
-    filename = os.path.join(DIR_PATH, filename+'.csv')
+    filename = os.path.join(FILES_PATH, filename+'.csv')
     with open(filename, 'wb') as f:
         writer = csv.writer(f)
         writer.writerows(rows)
+
+def harvest_ratio(pages, classifier, filename):
+    total = 0
+    relevant = 0
+
+    rows = [['Domain', '# Relevant pages', '# Downloaded pages', '# Harvest Ratio']]
+
+    for domain in pages.keys():
+        num_pages = len(pages[domain])
+        domain_relevant = 0
+
+        for p in pages[domain]:
+            if classifier.classify(p):
+                domain_relevant += 1
+
+        total += num_pages
+        relevant += domain_relevant
+
+        domain_hr = float(domain_relevant) / num_pages
+
+        rows.append([domain, domain_relevant, num_pages, domain_hr])
+
+    hr =  float(relevant) / total
+
+    rows.append(['Total', relevant, total, hr])
+
+    save_csv(filename, rows)
 
 def run():
     crawl(True, 'using-heuristic-pages.pickle')
@@ -79,15 +90,10 @@ def run():
     classifier = Classifier()
 
     pages = import_crawled_pages('using-heuristic-pages.pickle')
-    heuristic_hr = harvest_ratio(pages, classifier)
+    harvest_ratio(pages, classifier, 'heuristic_harvest_ratio_results')
 
     pages = import_crawled_pages('bfs-pages.pickle')
-    bfs_hr = harvest_ratio(pages, classifier)
-
-    del pages
-
-    rows = [['HR Heuristic', 'HR BFS'], [heuristic_hr, bfs_hr]]
-    save_csv('harvest_ratio_results', rows)
+    harvest_ratio(pages, classifier, 'bfs_harvest_ratio_results')
 
 if __name__ == '__main__':
     run()
